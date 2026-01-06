25f9ed8e433f62819c2919fa2d5273b4f4356426
ZakDav815 <673804@eastridingcollege.ac.uk>
2026-01-06 09:52:46 +0000

    nothing major
    trying to wrap head around how my files have been done

--- passwordHash.php
+++ passwordHash.php
@@ -3,7 +3,12 @@
 $plain_text = "PlaceholderPassword";
 echo $plain_text;
 
+// you will need to hash password strings sent in a form, as plain text, we use the input type password, to help with this.
+
 $hashed = password_hash(($plain_text, algo: PASSWORD_DEFAULT));
 
+//get using POST the value being sent
+
+//hash it and then send the hash to the DB
 
 ?>
